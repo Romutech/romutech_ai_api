79d5658651742321cb6bbb75a3a582f70d189af6
Robin Muller <robin.muller.romutech@gmail.com>
2021-03-14 00:29:58 +0100

    feature: creation of bulk create #3

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from .serializers import PhotoSerializer
 from rest_framework.viewsets import ModelViewSet
+from .mixins.rest import CreateListMixin
 from .models import Photo
 
 
-class PhotoViewSet(ModelViewSet):
+class PhotoViewSet(CreateListMixin, ModelViewSet):
     """
     API endpoint that allows photos to be viewed or edited.
     """
