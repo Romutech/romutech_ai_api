14dd332b31ad677ed1ae002c1e7a1f912cdd63af
Robin Muller <robin.muller.romutech@gmail.com>
2021-03-14 00:08:34 +0100

    feature: creation of photo model #3

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo_b64 = models.TextField()
+    created_at = models.DateField(default=timezone.now, verbose_name="Date creation")
+
+    class Meta:
+        verbose_name = "Photo"
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.photo_b64
