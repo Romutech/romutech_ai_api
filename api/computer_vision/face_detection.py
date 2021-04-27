7b3553904ba0bb7c4b301b1691df825668bf5779
Robin Muller <robin.muller.romutech@gmail.com>
2021-04-27 23:21:40 +0200

    wip

--- api/computer_vision/face_detection.py
+++ api/computer_vision/face_detection.py
@@ -22,7 +22,7 @@ def face_detection(p):
    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
    blackwhite = cv2.equalizeHist(gray)
    rects = cascade.detectMultiScale(
-      blackwhite, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
+      blackwhite, scaleFactor=1.1, minNeighbors=3, minSize=(1, 1),
       flags=cv2.CASCADE_SCALE_IMAGE)
    for x, y, w, h in rects:
       cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
